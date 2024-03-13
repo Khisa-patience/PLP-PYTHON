ab04e32902c096e82ce530ca9c6903bd1ac4c636
Khisa-patience <100830598+Khisa-patience@users.noreply.github.com>
2024-03-13 12:14:40 +0300

    Create user_input.py
    Intro To Python Assignment
    Objective: To learn how to take user input in Python

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Asking user for their name and storing it in a variable called "name"
+name = input("What is your name? ")
+
+# Asking user for their age and storing it in a variable called "age"
+age = input("What is your age? ")
+
+# Asking user for their location and storing it in a variable called "location"
+location = input("Where are you located? ")
+
+# Printing out a personalized message using user's name, age, and location
+print("Hello " + name + ", you are " + age + " years old and live in " + location + ".")
